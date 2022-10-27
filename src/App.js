cb972f280a42b28015eff57e3098dd1b55956b37
Maria Constance <93349200+constance03@users.noreply.github.com>
2022-10-27 15:27:12 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import pokemons from "./pokemon/pokemon.json";
+import PokemonCard from "./components/PokemonCard/PokemonCard";
+import { getColors } from "./utils/ReturnCardColor";
+import Header from "./components/Header/Header.js";
+const GlobalStyle = createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: "Inter", sans-serif;
+  
+  }
+`;
+const CardsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(440px, 1fr));
+  justify-items: center;
+`;
+function App() {
+  const [pesquisa, setPesquisa] = useState("");
+  const [idFilter, setIdFilter] = useState("");
+  const [tipo, setTipo] = useState("");
+  const [ordem, setOrdem] = useState("");
+
+  return (
+    <>
+      <GlobalStyle />
+      <Header
+        idFilter={idFilter}
+        setIdFilter={setIdFilter}
+        pesquisa={pesquisa}
+        setPesquisa={setPesquisa}
+        tipo={tipo}
+        setTipo={setTipo}
+        ordem={ordem}
+        setOrdem={setOrdem}
+      />
+      <CardsContainer>
+        {pokemons
+          .filter((pokemon) => {
+            return idFilter ? pokemon.id.includes(idFilter) : pokemon;
+          })
+          .filter((pokemon) => {
+            return pokemon.name.english
+              .toLowerCase()
+              .includes(pesquisa.toLowerCase());
+          })
+          .filter((pokemon) => {
+            return tipo ? pokemon.type.includes(tipo) : pokemon;
+          })
+          .sort((a, b) => {
+            if (ordem === "crescente") {
+              if (a.name.english > b.name.english) return 1;
+              if (a.name.english < b.name.english) return -1;
+            } else if (ordem === "decrescente") {
+              if (a.name.english < b.name.english) return 1;
+              if (a.name.english > b.name.english) return -1;
+            } else {
+              return 0;
+            }
+          })
+          .map((pokemon) => {
+            return (
+              <PokemonCard
+                cardColor={getColors(pokemon.type[0])}
+                key={pokemon.id}
+                pokemon={pokemon}
+              />
+            );
+          })}
+      </CardsContainer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { Container } from "./styles";
+
+const Header = (props) => {
+  const pokemontypesArray = [
+    "Normal",
+    "Fire",
+    "Water",
+    "Grass",
+    "Flying",
+    "Fighting",
+    "Poison",
+    "Electric",
+    "Ground",
+    "Rock",
+    "Psychic",
+    "Ice",
+    "Bug",
+    "Ghost",
+    "Steel",
+    "Dragon",
+    "Dark",
+    "Fairy"
+  ];
+
+  const handleSearch = (e) => {
+    props.setPesquisa(e.target.value);
+  };
+
+  const handleIdSearch = (e) => {
+    props.setIdFilter(e.target.value);
+  };
+
+  const buscarTipo = (e) => {
+    props.setTipo(e.target.value);
+  };
+
+  const crescenteDecrescente = (e) => {
+    props.setOrdem(e.target.value);
+  };
+
+  return (
+    <Container>
+      <input
+        type="number"
+        placeholder="Buscar por id"
+        onChange={handleIdSearch}
+        value={props.idFilter}
+      />
+      <input
+        type="text"
+        placeholder="Buscar por nome"
+        onChange={handleSearch}
+        value={props.pesquisa}
+      />
+      <select onChange={crescenteDecrescente}>
+        <option value="normal">Ordenar</option>
+        <option value="crescente">Crescente</option>
+        <option value="decrescente">Decrescente</option>
+      </select>
+      <select name="tipo" id="tipo" onChange={buscarTipo}>
+        <option value="">Selecione um tipo</option>
+        {pokemontypesArray.map((type) => {
+          return (
+            <option key={type} value={type}>
+              {type}
+            </option>
+          );
+        })}
+      </select>
+    </Container>
+  );
+};
+
+export default Header;
